e1928b4623c95567c25abd8bed83502eff2cb55b
Himanshu Gaur <himanshugaur@Himanshus-MacBook-Air.local>
2024-04-24 20:33:40 +0530

    reading sms using telephony

--- spend-manager-mobile-app/modules/android-sms-manager/android/src/main/java/expo/modules/androidsmsmanager/AndroidSmsManagerModule.kt
+++ spend-manager-mobile-app/modules/android-sms-manager/android/src/main/java/expo/modules/androidsmsmanager/AndroidSmsManagerModule.kt
@@ -2,6 +2,7 @@ package expo.modules.androidsmsmanager
 
 import android.Manifest
 import android.net.Uri
+import android.provider.Telephony
 import expo.modules.kotlin.modules.Module
 import expo.modules.kotlin.modules.ModuleDefinition
 import expo.modules.interfaces.permissions.Permissions
@@ -9,79 +10,84 @@ import expo.modules.kotlin.Promise
 import expo.modules.kotlin.exception.Exceptions
 
 class AndroidSmsManagerModule : Module() {
-  // Each module class must implement the definition function. The definition consists of components
-  // that describes the module's functionality and behavior.
-  // See https://docs.expo.dev/modules/module-api for more details about available components.
-  private val permissionsManager: Permissions
-    get() = appContext.permissions ?: throw Exceptions.PermissionsModuleNotFound()
-  override fun definition() = ModuleDefinition {
-    // Sets the name of the module that JavaScript code will use to refer to the module. Takes a string as an argument.
-    // Can be inferred from module's class name, but it's recommended to set it explicitly for clarity.
-    // The module will be accessible from `requireNativeModule('AndroidSmsManager')` in JavaScript.
-    Name("AndroidSmsManager")
+    // Each module class must implement the definition function. The definition consists of components
+    // that describes the module's functionality and behavior.
+    // See https://docs.expo.dev/modules/module-api for more details about available components.
+    private val permissionsManager: Permissions
+        get() = appContext.permissions ?: throw Exceptions.PermissionsModuleNotFound()
 
-    // Sets constant properties on the module. Can take a dictionary or a closure that returns a dictionary.
-    Constants(
-            "PI" to Math.PI
-    )
+    override fun definition() = ModuleDefinition {
+        // Sets the name of the module that JavaScript code will use to refer to the module. Takes a string as an argument.
+        // Can be inferred from module's class name, but it's recommended to set it explicitly for clarity.
+        // The module will be accessible from `requireNativeModule('AndroidSmsManager')` in JavaScript.
+        Name("AndroidSmsManager")
 
-    // Defines event names that the module can send to JavaScript.
-    Events("onChange")
+        // Sets constant properties on the module. Can take a dictionary or a closure that returns a dictionary.
+        Constants(
+                "PI" to Math.PI
+        )
 
-    // Defines a JavaScript synchronous function that runs the native code on the JavaScript thread.
-    Function("hello") {
-      "Hello Wordly!!!! 👋"
+        // Defines event names that the module can send to JavaScript.
+        Events("onChange")
+
+        // Defines a JavaScript synchronous function that runs the native code on the JavaScript thread.
+        Function("hello") {
+            "Hello Wordly!!!! 👋"
+        }
+
+        // Defines a JavaScript function that always returns a Promise and whose native code
+        // is by default dispatched on the different thread than the JavaScript runtime runs on.
+        AsyncFunction("setValueAsync") { value: String ->
+            // Send an event to JavaScript.
+            sendEvent("onChange", mapOf(
+                    "value" to value
+            ))
+        }
+
+        // Enables the module to be used as a native view. Definition components that are accepted as part of
+        // the view definition: Prop, Events.
+        View(AndroidSmsManagerView::class) {
+            // Defines a setter for the `name` prop.
+            Prop("name") { view: AndroidSmsManagerView, prop: String ->
+                println(prop)
+            }
+        }
+
+        AsyncFunction("requestPermissionsAsync") { promise: Promise ->
+            Permissions.askForPermissionsWithPermissionsManager(permissionsManager, promise, Manifest.permission.READ_SMS, Manifest.permission.RECEIVE_SMS)
+        }
+
+        AsyncFunction("getPermissionsAsync") { promise: Promise ->
+            Permissions.getPermissionsWithPermissionsManager(permissionsManager, promise, Manifest.permission.READ_SMS, Manifest.permission.RECEIVE_SMS)
+        }
+
+        AsyncFunction("getSmsAsync") {
+            ensureReadPermission()
+            val smsList = ArrayList<SmsInfo>()
+            val contentResolver = appContext.currentActivity?.contentResolver
+            val cursor = contentResolver?.query(Telephony.Sms.Inbox.CONTENT_URI, null, null, null, null)
+            cursor?.moveToFirst()
+            val maxMessageToIterate = 10
+            while (cursor?.moveToNext() == true && smsList.size < maxMessageToIterate) {
+                val id = cursor.getString(cursor.getColumnIndexOrThrow(Telephony.Sms._ID))
+                val receivedFrom = cursor.getString(cursor.getColumnIndexOrThrow(Telephony.Sms.CREATOR))
+                val date = cursor.getString(cursor.getColumnIndexOrThrow(Telephony.Sms.DATE))
+                val subject = cursor.getString(cursor.getColumnIndexOrThrow(Telephony.Sms.SUBJECT))
+                val body = cursor.getString(cursor.getColumnIndexOrThrow(Telephony.Sms.BODY))
+
+                val sms = SmsInfo(id, receivedFrom, date, subject, body)
+                smsList.add(sms)
+            }
+            cursor?.close()
+        }
+
+
     }
 
-    // Defines a JavaScript function that always returns a Promise and whose native code
-    // is by default dispatched on the different thread than the JavaScript runtime runs on.
-    AsyncFunction("setValueAsync") { value: String ->
-      // Send an event to JavaScript.
-      sendEvent("onChange", mapOf(
-              "value" to value
-      ))
+    private fun ensureReadPermission() {
+        val hasPermission = permissionsManager.hasGrantedPermissions(Manifest.permission.READ_SMS)
+        if (!hasPermission) {
+            throw Exceptions.MissingPermissions(Manifest.permission.READ_SMS)
+        }
     }
-
-    // Enables the module to be used as a native view. Definition components that are accepted as part of
-    // the view definition: Prop, Events.
-    View(AndroidSmsManagerView::class) {
-      // Defines a setter for the `name` prop.
-      Prop("name") { view: AndroidSmsManagerView, prop: String ->
-        println(prop)
-      }
-    }
-
-    AsyncFunction("requestPermissionsAsync") { promise: Promise ->
-      Permissions.askForPermissionsWithPermissionsManager(permissionsManager, promise, Manifest.permission.READ_SMS, Manifest.permission.RECEIVE_SMS)
-    }
-
-    AsyncFunction("getPermissionsAsync") { promise: Promise ->
-      Permissions.getPermissionsWithPermissionsManager(permissionsManager, promise, Manifest.permission.READ_SMS, Manifest.permission.RECEIVE_SMS)
-    }
-
-    AsyncFunction("getSmsAsync"){promise: Promise->
-      ensureReadPermission()
-      val smsList= ArrayList<SmsInfo>()
-      val smsUri=Uri.parse("content://sms/inbox")
-      val cursor=appContext.currentActivity?.contentResolver?.query(smsUri,arrayOf("_id", "address", "date", "body"), null, null, null)
-      cursor?.moveToFirst()
-      while(cursor?.moveToNext()==true){
-        val id=cursor.getString(0)
-        val receivedFrom=cursor.getString(1)
-        val date=cursor.getString(2)
-        val body =cursor.getString(3)
-        val sms=SmsInfo(id,receivedFrom,date,body)
-        smsList.add(sms)
-      }
-      cursor?.close()
-    }
-
-
-  }
-  private fun ensureReadPermission() {
-    val hasPermission = permissionsManager.hasGrantedPermissions(Manifest.permission.READ_SMS)
-    if (!hasPermission) {
-      throw Exceptions.MissingPermissions(Manifest.permission.READ_SMS)
-    }
-  }
 }
--- spend-manager-mobile-app/modules/android-sms-manager/android/src/main/java/expo/modules/androidsmsmanager/SmsInfo.kt
+++ spend-manager-mobile-app/modules/android-sms-manager/android/src/main/java/expo/modules/androidsmsmanager/SmsInfo.kt
@@ -1,3 +1,3 @@
 package expo.modules.androidsmsmanager
 
-class SmsInfo(var id:String, var from: String, var date: String,  var body: String)
+class SmsInfo(var id:String, var from: String, var date: String,  var subject:String, var body: String)
