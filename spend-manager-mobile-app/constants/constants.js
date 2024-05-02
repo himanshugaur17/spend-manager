a1f86adaaab923d88924a85d884fba34df926799
Himanshu Gaur <himanshugaur@Himanshus-MacBook-Air.local>
2024-05-02 20:35:57 +0530

    define regex for messages

---
+++ spend-manager-mobile-app/constants/constants.js
@@ -0,0 +1,14 @@
+const senders = [
+  {
+    from: ["BV-CBSSBI", "BZ-CBSSBI", "VM-CBSSBI", "BP-CBSSBI"],
+    metadata: ["has a debit by NACH of Rs XXX on Date", "of INR XXX deducted"],
+  },
+  {
+    from: ["JM-AUBANK", "JD-AUBANK", "JK-AUBANK", "VD-AUBANK"],
+    metadata: ["INR XXX spent on your AU .* at MERCHANTYYY on Date"],
+  },
+  {
+    from: ["VM-SBIUPI"],
+    metadata: ["debited by XXX on date Date trf to MERCHANTYYY Refno REFZZZ"],
+  },
+];
